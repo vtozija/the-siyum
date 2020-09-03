002034b5143490cf974a3675523a67bf2109c91e
Vedrana Tozija <vedrana.tozija@gmail.com>
2020-09-03 22:48:51 +0200

    frontend layout change

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 require('./bootstrap');
+require('./stripe');
 
 $(function() {
     $('.nav-btn').click(function() {
@@ -8,8 +9,9 @@ $(function() {
 
     $('#countries').on('change', function() {
         if(this.value == 'USA') {
-            console.log('correct');
             $('.states-block').show();
+        } else {
+            $('.states-block').hide();
         }
     });
 
@@ -27,59 +29,6 @@ $(function() {
         }
 
         document.getElementById("timer").innerHTML = Math.floor(count / 60) + ":" + count % 60;
-    }
-
-
-    var $form = $(".require-validation");
-
-    $('form.require-validation').bind('submit', function(e) {
-        e.preventDefault();
-        var $form = $(".require-validation"),
-            inputSelector = ['input[type=email]', 'input[type=password]',
-                'input[type=text]', 'input[type=file]',
-                'textarea'
-            ].join(', '),
-            $inputs = $form.find('.required').find(inputSelector),
-            $errorMessage = $form.find('div.error'),
-            valid = true;
-        $errorMessage.addClass('hide');
-
-        $('.has-error').removeClass('has-error');
-        $inputs.each(function(i, el) {
-            var $input = $(el);
-            if ($input.val() === '') {
-                $input.parent().addClass('has-error');
-                $errorMessage.removeClass('hide');
-                e.preventDefault();
-            }
-        });
-
-        if (!$form.data('cc-on-file')) {
-            e.preventDefault();
-            Stripe.setPublishableKey($form.data('stripe-publishable-key'));
-            Stripe.createToken({
-                number: $('.card-number').val(),
-                cvc: $('.card-cvc').val(),
-                exp_month: $('.card-expiry-month').val(),
-                exp_year: $('.card-expiry-year').val()
-            }, stripeResponseHandler);
-        }
-
-    });
-
-    function stripeResponseHandler(status, response) {
-        if (response.error) {
-            $('.error')
-                .removeClass('hide')
-                .find('.alert')
-                .text(response.error.message);
-        } else {
-            /* token contains id, last4, and card type */
-            var token = response['id'];
-            $form.find('input[type=text]').empty();
-            $form.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
-            $form.get(0).submit();
-        }
     }
 
 });
